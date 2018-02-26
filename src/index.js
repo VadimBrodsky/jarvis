a82f1d74f6a3a0d6b535c71a636a572c4f6d08ac
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-02-26 14:43:54 -0500

    Add polyfills

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import 'core-js/fn/array/includes';
+import 'core-js/fn/object/entries';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import 'raf/polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
