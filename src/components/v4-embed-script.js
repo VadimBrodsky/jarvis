1f54ffd14f564fdca0e4ec32e3e62f40630a3a5a
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-06-18 16:04:04 -0400

    Add preview=1 to V4 embed script

--- src/components/v4-embed-script.js
+++ src/components/v4-embed-script.js
@@ -6,7 +6,7 @@ class V4EmbedScript extends React.Component {
 
     const script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = `//${domain}.vidyard.${env}${js}/embed/v4.js`;
+    script.src = `//${domain}.vidyard.${env}${js}/embed/v4.js?preview=1`;
     script.async = true;
 
     this.refs.inlineScript.appendChild(script);
