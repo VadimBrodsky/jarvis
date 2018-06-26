70fb5535da29cd52bf1ae80e739446c68efa8ac1
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-06-26 11:21:28 -0400

    Set playbackurl data attrbute

--- src/components/v4-embed-script.js
+++ src/components/v4-embed-script.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 class V4EmbedScript extends React.Component {
   componentDidMount() {
-    const { domain, env, js } = this.props;
+    const { domain, env, js, playbackurl } = this.props;
 
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = `//${domain}.vidyard.${env}${js}/embed/v4.js?preview=1`;
     script.async = true;
+    script.setAttribute('data-playbackurl', `${domain}.vidyard.${env}`);
 
     this.refs.inlineScript.appendChild(script);
   }
